966817541ad6d530025c8071220cdbf396fbbdfe
Andrew Jones <ajones@digitalbazaar.com>
2021-11-17 09:01:03 -0500

    Add LICENSE.md, start of binary, & package.json.

---
+++ binary.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+/**
+ * Bedrock Ledger Maintainer CLI
+ * Command line interface for updating Witness Pool documents
+ *
+ * Copyright (C) 2021 Digital Bazaar, Inc. All Rights Reserved.
+ */
